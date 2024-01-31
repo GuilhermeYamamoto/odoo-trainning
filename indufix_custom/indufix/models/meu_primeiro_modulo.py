c99ceba2c50614167c62773c6aa39b9ec29e78f3
Guilherme <guilherme.yamamoto@indufix.com.br>
2024-01-31 13:47:47 -0300

    testing

---
+++ indufix_custom/indufix/models/meu_primeiro_modulo.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class MeuPrimeiroModulo(models.Model):
+    _name = 'meu.primeiro.modelo'
+    _description = 'meu primeiro modelo criado no odoo'
+
+    campo1 = fields.Integer(string="Campo 1")
+
